fd1eb1a84d8298bc028ceb3f1a07445b5dc7c123
pranavbagade <138678798+pranavbagade@users.noreply.github.com>
2024-07-04 13:40:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-var http = require('http');
 
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('A Monk in Cloud'); //write a response to the client
-  res.end(); //end the response
-}).listen(80); //the server object listens on port 80
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+};
+
+https.createServer(options, function (req, res) {
+  res.writeHead(200);
+  res.end('A Monk in Cloud');
+}).listen(443); // HTTPS default port
+
+console.log('Server running at https://localhost:443/');
